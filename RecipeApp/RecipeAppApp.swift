a015433b5b56c55981b3f84da1a7d543ed3735fa
EricTMcode <93444335+EricTMcode@users.noreply.github.com>
2023-06-17 14:12:18 +0200

    Initial Commit

---
+++ RecipeApp/RecipeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeAppApp.swift
+//  RecipeApp
+//
+//  Created by Eric on 17/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct RecipeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
